dcc5dd24d04dbc470111b2be244c3ce3c1faf009
Ar3secchim <renarasecchim@gmail.com>
2023-04-12 17:24:58 -0300

    chore: config eslint mobile and server

--- apps/server/index.js
+++ apps/server/index.js
@@ -1,5 +1,5 @@
-import {routes} from './routes.js';
-import express  from 'express';
+import { routes } from './routes.js'
+import express from 'express'
 
 const app = express()
 const port = 3333
@@ -7,6 +7,6 @@ const port = 3333
 app.use(express.json())
 app.use(routes)
 
-app.listen({port}, () => {
-  console.log(`🔥 Server running on port http://localhost:${port}`);
+app.listen({ port }, () => {
+  console.log(`🔥 Server running on port http://localhost:${port}`)
 })
