dbcd3d126d2566f7a731469b8f00b8a95067b1cc
Renara Secchim <renarasecchim@DELL-ANELISE.localdomain>
2023-04-13 14:20:01 -0300

    chore: test

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native'
 
 export default function App() {
   return (
-    <View className=" flex-1 bg-zinc-900 items-center justify-center">
+    <View className=" flex-1 bg-zinc-900 items-center ">
       <Text className="text-white text-4xl font-bold">Hellou World!!</Text>
       <StatusBar style="light" />
     </View>
