46021bbcc6c97607423a899d887ae0f0ddddfb6b
Ar3secchim <renarasecchim@gmail.com>
2023-04-13 18:47:33 -0300

    feat: config themes tailwind native base

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      'green': '#538177',
+      'gray': '#222222',
+      'gray-light': '#d3dce6',
+      'white': '#ffffff',
+      'black': '#000000',
+
+    },
+    fontFamily: {
+      sans: ['Poppis', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
     extend: {},
   },
-  plugins: [],
+    plugins: [],
+  },
 }
