a9414abd2712495d0502057a7771233bf749e3fb
renarasecchim <renarasecchim@gmail.com>
2023-04-19 17:36:56 -0300

    feat: update  config tailwind

--- apps/mobile/tailwind.config.js
+++ apps/mobile/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     colors: {
-      green: "#538177",
-      gray: "#222222",
-      "gray-light": "#d3dce6",
-      white: "#ffffff",
-      black: "#000000",
+      green: '#538177',
+      gray: '#222222',
+      'gray-light': '#d3dce6',
+      white: '#ffffff',
+      black: '#000000',
     },
     fontFamily: {
-      sans: ["Poppis", "sans-serif"],
-      serif: ["Merriweather", "serif"],
+      sans: ['Poppis', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
       extend: {},
     },
     plugins: [],
