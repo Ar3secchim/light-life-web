89ddbaa241e9082cb39029a52d0fc72575be0520
renarasecchim <renarasecchim@gmail.com>
2023-06-23 16:56:59 -0300

    chore(mobile): text navBar

--- apps/mobile/src/Pages/App.jsx
+++ apps/mobile/src/Pages/App.jsx
@@ -1,9 +1,21 @@
+import { AiOutlineHome, AiOutlineBulb, AiOutlineProfile } from 'react-icons/ai'
 function App() {
   return (
     <>
       <span className="font-poppins flex items-center text-3xl text-primary">
         Hellou Word!!
       </span>
+      <div className="btm-nav">
+        <button>
+          <AiOutlineHome />
+        </button>
+        <button className="active">
+          <AiOutlineBulb />
+        </button>
+        <button>
+          <AiOutlineProfile />
+        </button>
+      </div>
     </>
   )
 }
