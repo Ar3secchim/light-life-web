f2ca4a4398dd5f432bc8320dfcb52d0a0508b828
renarasecchim <renarasecchim@gmail.com>
2023-06-23 17:05:11 -0300

    chore(mobile): text navBar

--- apps/mobile/src/Pages/App.jsx
+++ apps/mobile/src/Pages/App.jsx
@@ -2,7 +2,7 @@ import { AiOutlineHome, AiOutlineBulb, AiOutlineProfile } from 'react-icons/ai'
 function App() {
   return (
     <>
-      <span className="font-poppins flex items-center text-3xl text-primary">
+      <span className=" w-8 font-poppins flex items-center text-3xl text-primary">
         Hellou Word!!
       </span>
       <div className="btm-nav">
