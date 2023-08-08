1cde29ac86a535181ee702524bb21860f8a21ae8
Ar3secchim <renarasecchim@gmail.com>
2023-08-08 17:50:44 -0300

    chore: updata config package

--- apps/mobile/vite.config.js
+++ apps/mobile/vite.config.js
@@ -4,27 +4,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: 'autoUpdate',
-    manifest:{
-      icons:[
-        {
-          src: "/public/favicon.ico",
-          sizes: "64x64 32x32 24x24 16x16",
-          type: "image/x-icon"
-        },
-        {
-          src: "/public/LogoVector.png",
-          type: "image/png",
-          sizes: "192x192"
-        },
-        {
-          src: "/public/LogoVector512.png",
-          type: "image/png",
-          sizes: "512x512"
-        }
-      ]
-    }
-  })],
+  plugins: [react(), VitePWA({ registerType: 'autoUpdate'})],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
