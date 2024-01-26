3b417c311b21f6022937b32ad708119be064cfa0
Ar3secchim <renarasecchim@gmail.com>
2024-01-26 12:28:51 -0300

    feature(design): create page into and page register

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,34 @@
+import Image from "next/image";
+import Ilustration from "@/assets/SignIn/ilustratorPageInto.png";
+import IlustrationElipse from "@/assets/SignIn/elipse-superior.svg";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello word</h1>
+    <main>
+      <div className="absolute -left-2">
+        <Image src={IlustrationElipse} alt="elipse" />
+      </div>
+      <section className="flex flex-col justify-center h-screen">
+        <Image
+          src={Ilustration}
+          alt="jovem sentado no sofá com um gato ao lado dele"
+        />
+
+        <div className="pb-14 pt-6">
+          <h1 className="font-bold text-xl text-center pb-4">
+            Faça as coisas com ToDo
+          </h1>
+          <p className="text-base text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipisicing. Maxime,
+            tempore! Animi nemo aut atque, deleniti nihil dolorem repellendus.
+          </p>
+        </div>
+        <Button asChild>
+          <Link href="/register">Vamos começar</Link> 
+        </Button>
+      </section>
     </main>
   );
 }
-
