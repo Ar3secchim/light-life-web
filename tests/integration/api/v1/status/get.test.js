5b343d8a2f55b1d98b83c674e12e03d37d643640
Renara Secchim <renarasecchim@gmail.com>
2024-05-30 19:45:40 -0300

    test: :test_tube: add test for route `/status`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,18 @@ describe("should to request /api/v1/status return status", () => {
     const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
   });
+
+  test("should body response correct", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+
+    const responseBody = await response.json();
+    const parseDate = new Date(responseBody.updated_at).toISOString();
+    expect(responseBody.updated_at).toBeDefined();
+
+    expect(responseBody.updated_at).toEqual(parseDate);
+    expect(responseBody.dependencies.database.version).toEqual("16.0");
+    expect(responseBody.dependencies.database.status).toEqual("healthy");
+    expect(responseBody.dependencies.database.max_connections).toEqual(100);
+    expect(responseBody.dependencies.database.opened_connections).toEqual(1);
+  });
 });
