bc5f247e856b18e622b14cdc5585a723e53004aa
Renara Secchim <renarasecchim@gmail.com>
2024-05-30 14:53:25 -0300

    refactor: :wrench: config database for docker

--- pages/api/v1/status.js
+++ pages/api/v1/status.js
@@ -1,5 +1,9 @@
-function Status(request, responde) {
+import database from "../../../infra/database";
+
+async function status(request, responde) {
+  const result = await database.query("SELECT 1 + 1;")
+  console.log(result.rows)
   responde.status(200).json({ status: "Tudo Funcionando", code: 200 });
 }
 
-export default Status;
+export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,7 +1,6 @@
-describe("should to request /api/v1/status return status", ()=>{
-  test("GET return status 200", async ()=>{
-    const response = await fetch("http://localhost:3000/api/v1/status")
-
+describe("should to request /api/v1/status return status", () => {
+  test("GET return status 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
     expect(response.status).toBe(200);
-  })
-})
+  });
+});
