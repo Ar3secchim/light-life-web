498ec30005e9ca87e9915aa4fd7aed5e835458f2
Renara Secchim <renarasecchim@gmail.com>
2024-09-20 11:39:23 -0300

    Refactor: Add vite-plugin-pwa to package.json and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'node:path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),svgr()],
+  plugins: [react(),svgr(),   VitePWA({
+    registerType: 'autoUpdate',
+  })],
   server: {
     open: true,
     port: 3000,
@@ -19,3 +22,4 @@ export default defineConfig({
     },
   },
 })
+
